f1a29ec984ad99f4514a55ce9c86da74482beb6d
Eduardo Ferreira de Lima <52423589+Eduardo191@users.noreply.github.com>
2020-06-04 16:39:49 -0300

    Refatorando páginas com nunjucks

--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
 const port = 3000
 
 server.use(express.json())
+server.use(express.static('public'))
+
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true
+})
+
+server.get('/', (req, res) => {
+  return res.render('index.html', { title: 'Seu marketplace de coleta de resíduos' })
+})
+
+server.get('/create-point', (req, res) => {
+  return res.render('create-point.html')
+})
+
+server.get('/search', (req, res) => {
+  return res.render('search-results.html')
+})
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`)
